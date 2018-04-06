a1d37e81e3816d3a66f0191fbd7f374a63219b8b
dk <dk@aehost>
2018-04-06 09:32:42 +0200

    move config files to config/

--- install_software.sh
+++ install_software.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo Install software 1
+echo Run after reboot
+echo Install software... 
 sudo pacman -Syu
 sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xinput xf86-video-intel acpid reflector firefox i3 rxvt-unicode wget unarj lzip lzop unrar unzip p7zip 
 wget https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
@@ -21,3 +22,5 @@ rm -rf yaourt*
 yaourt -S --noconfirm tk
 git config --global user.email dk@aehost
 git config --global user.name dk
+
+yaourt -S --noconfirm 
