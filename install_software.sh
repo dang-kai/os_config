6c849634bd781e50afdc7e214233841b958eb509
dk <dk@AEHost9>
2018-04-08 00:14:33 +0200

    more config

--- install_software.sh
+++ install_software.sh
@@ -3,7 +3,7 @@
 echo Run after reboot
 echo Install software... 
 sudo pacman -Syu
-sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xinput xf86-video-intel acpid reflector firefox i3 rxvt-unicode wget unarj lzip lzop unrar unzip p7zip 
+sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xinput xf86-video-intel acpid reflector firefox i3 wget unarj lzip lzop unrar unzip p7zip 
 wget https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
 wget https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz
 tar -xf package-query.tar.gz
@@ -19,12 +19,22 @@ sudo pacman -U *.xz
 cd ..
 rm -rf yaourt*
 
-yaourt -S --noconfirm tk
+yaourt -S --noconfirm tk rxvt-unicode-patched
 
 # install fonts
-yaourt -S --noconfirm ttf-dejavu ttf-ubuntu-font-family noto-fonts noto-fonts-cjk adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts
+yaourt -S --noconfirm ttf-dejavu ttf-ubuntu-font-family noto-fonts noto-fonts-cjk adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts wqy-microhei
 
 # copy config files
 cp config/bashrc ~/.bashrc
-cp -r i3 ~/.i3
-cp -r 
+cp config/vimrc ~/.vimrc
+cp config/xinitrc ~/.xinitrc
+cp config/Xresources ~/.Xresources
+cp config/i3blocks.conf ~/.i3blocks.conf
+cp -r config/i3 ~/.i3
+sudo cp tmux.conf /etc/tmux.conf
+sudo cp local.conf /etc/fonts/local.conf
+
+# install software
+yaourt -S --noconfirm uget transmission-gtk openssh
+yaourt -S --noconfirm fcitx fcitx-sunpinyin fcitx-configtool fcitx bc
+yaourt -S --noconfirm dropbox google-chrome
