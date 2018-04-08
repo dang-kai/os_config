4760d1c811912593dfa1a72de66fe6deeb094ab7
dk <dk@aehost>
2018-04-09 00:46:03 +0200

    fix

--- install_software.sh
+++ install_software.sh
@@ -19,7 +19,7 @@ sudo pacman -U *.xz
 cd ..
 rm -rf yaourt*
 
-yaourt -S --noconfirm tk rxvt-unicode-patched redshift
+yaourt -S --noconfirm tk rxvt-unicode-patched redshift sysstat dmenu
 
 # install fonts
 yaourt -S --noconfirm ttf-dejavu ttf-ubuntu-font-family noto-fonts noto-fonts-cjk adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts wqy-microhei
@@ -31,13 +31,18 @@ cp config/xinitrc ~/.xinitrc
 cp config/Xresources ~/.Xresources
 cp config/i3blocks.conf ~/.i3blocks.conf
 cp -r config/i3 ~/.i3
-sudo cp tmux.conf /etc/tmux.conf
-sudo cp local.conf /etc/fonts/local.conf
+cp -r config/fcitx ~/.config/
+echo 'Manually add sunpinyin to the input method list.'
+sudo cp config/tmux.conf /etc/tmux.conf
+sudo cp config/local.conf /etc/fonts/local.conf
+sudo cp config/makepkg.conf /etc/makepkg.conf
 
 # install software
 yaourt -S --noconfirm uget transmission-qt openssh
 yaourt -S --noconfirm fcitx fcitx-sunpinyin fcitx-configtool fcitx bc
 cp -r config/fcitx/ ~/.config/
-yaourt -S kwrite spectacle vlc
+yaourt -S --noconfirm kwrite spectacle vlc meld
 yaourt -S --noconfirm dropbox google-chrome
 
+# for virtualbox guest system
+echo 'Install virtualbox-guest-utils if the system is running on virtualbox.'
