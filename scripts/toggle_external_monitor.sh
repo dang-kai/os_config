aaa495d7cda988804b70a528f942ba8191a3b026
Kai Dang <dk@AEHost1.localdomain>
2021-02-19 23:14:38 +0800

    save

--- scripts/toggle_external_monitor.sh
+++ scripts/toggle_external_monitor.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-EXTERNAL_OUTPUT="HDMI-2"
-INTERNAL_OUTPUT="eDP-1"
+EXTERNAL_OUTPUT="HDMI2"
+INTERNAL_OUTPUT="eDP1"
 
 # if we don't have a file, start at zero
 if [ ! -f "/tmp/monitor_mode.dat" ] ; then
-  monitor_mode="all"
+  monitor_mode="ALL"
 
 # otherwise read the value from the file
 else
-  monitor_mode=`cat /tmp/monitor_mode.dat`
+  monitor_mode=$(cat /tmp/monitor_mode.dat)
 fi
 
 if [ $monitor_mode = "all" ]; then
@@ -18,10 +18,10 @@ elif [ $monitor_mode = "EXTERNAL" ]; then
         monitor_mode="INTERNAL"
         xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --off
 elif [ $monitor_mode = "INTERNAL" ]; then
-        monitor_mode="CLONES"
+        monitor_mode="CLONE"
         xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --same-as $INTERNAL_OUTPUT
 else
-        monitor_mode="all"
+        monitor_mode="ALL"
         xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --right-of $INTERNAL_OUTPUT
 fi
 echo "${monitor_mode}" > /tmp/monitor_mode.dat
